668c5f53f41b159c21cdece0cf33f4133ab0edaa
Damian Kolczyński <damian.kolczynski@gmail.com>
2019-01-19 12:58:36 +0100

    Zabawa Wątkami2

--- src/pl/sda/watki/Main.java
+++ src/pl/sda/watki/Main.java
@@ -1,8 +1,53 @@
 package pl.sda.watki;
 
+import java.util.Random;
+
+class Runner implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 500; i++) {
+            System.out.println("Hello : " + i+ "thread: " +Thread.currentThread().getName());
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
+       // Thread thread = new Thread(new Runner());
+       // Thread thread2 = new Thread(new Runner());
+      //  thread.start();
+      //  thread2.start();
+
+        for (int i = 0; i < 5000; i++) {
+            new RunnerExample(i).start();
+        }
+       // RunnerExample runnerExample = new RunnerExample();
+       // RunnerExample runnerExample2 = new RunnerExample();
+       // runnerExample.start();
+       // runnerExample2.start();
+
+
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 500; i++) {
+                    System.out.println("Hello : " + i + "thread: " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        //thread.start();
 	// write your code here
     }
 }
--- src/pl/sda/watki/RunnerExample.java
+++ src/pl/sda/watki/RunnerExample.java
@@ -1,4 +1,33 @@
 package pl.sda.watki;
 
-public class RunnerExample {
+import java.util.Random;
+
+public class RunnerExample extends Thread {
+    private int num;
+    @Override
+    public void run(){
+        System.out.println(" Num = " + getNum() );
+
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public int getNum() {
+        return 2*num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public RunnerExample(int num) {
+        this.num = num;
+    }
 }
+
+
